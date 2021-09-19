50a543ae4b1f26a3aa1e8225ef925d6c5f90bb6b
laye-boly <layeboly58@gmail.com>
2021-09-19 15:17:33 +0000

    nullable attribute added to vaccine_appointements.vaccine_schedule_id and vaccine_appointements.patient_id

---
+++ database/migrations/2021_09_19_151533_add_nullable_to_vaccine_appointements.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNullableToVaccineAppointements extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('vaccine_appointements', function (Blueprint $table) {
+            $table->integer("vaccine_schedule_id")->nullable();
+            $table->integer("patient_id")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('vaccine_appointements', function (Blueprint $table) {
+            //
+        });
+    }
+}
