9afbecb122c1b49606c6ad17ea61233ff8dca177
laye-boly <layeboly58@gmail.com>
2021-09-19 15:58:30 +0000

    nullable attribute added to vaccine_appointements.vaccine_schedule_id and vaccine_appointements.patient_id

--- database/migrations/2021_09_19_155644_drop_vaccine_schedule_id_and_patient_id_from_vaccine_appointements.php
+++ database/migrations/2021_09_19_155644_drop_vaccine_schedule_id_and_patient_id_from_vaccine_appointements.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class DropeVaccineScheduleIdAndPatientIdVaccineAppointements extends Migration
+class DropVaccineScheduleIdAndPatientIdFromVaccineAppointements extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2021_09_19_155735_add_vaccine_schedule_id_and_patient_id_to_vaccine_appointements.php
+++ database/migrations/2021_09_19_155735_add_vaccine_schedule_id_and_patient_id_to_vaccine_appointements.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddPatientIdVaccineScheduleIdToVaccineAppointements extends Migration
+class AddVaccineScheduleIdAndPatientIdToVaccineAppointements extends Migration
 {
     /**
      * Run the migrations.
